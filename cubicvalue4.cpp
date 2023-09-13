6f5bb5e1a5a017ddb74744b46ae7b2670a9bddd7
dhruviksutariya <142383055+dhruviksutariya@users.noreply.github.com>
2023-09-13 09:53:41 +0530

    Add files via upload

---
+++ cubicvalue4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,cubic;
+	cout<<"enter the value of i"<<endl;
+	cin>>i;
+	cubic=i*i*i;
+	cout<<"ans of the cubic"<<cubic<<endl;
+	return 0;
+}
